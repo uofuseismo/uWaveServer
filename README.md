2d4cd9552210603362d2dea78c50127c886a3b38
Ben Baker <bakerb845@gmail.com>
2026-03-03 14:40:39 -0700

    beginning incorporation of grpc

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # uWaveServer
+
 A wave server that leverages Postgres and TimeScaleDB.
+
+# API
+
+To get the gRPC API you can do the following
+
+    git subtree add --prefix uWaveServerAPI https://github.com/uofuseismo/uWaveServerAPI.git main --squash
--- lib/dataClient/grpc.cpp
+++ lib/dataClient/grpc.cpp
@@ -3,11 +3,173 @@
 #include <spdlog/spdlog.h>
 #include "uWaveServer/dataClient/grpc.hpp"
 #include "uWaveServer/dataClient/grpcOptions.hpp"
+#include "uDataPacketServiceAPI/v1/broadcast.grpc.pb.h"
 
 using namespace UWaveServer::DataClient;
 
 namespace
 {
+
+class CustomAuthenticator : public grpc::MetadataCredentialsPlugin
+{
+public:    
+    CustomAuthenticator(const grpc::string &token) :
+        mToken(token)
+    {
+    }
+    grpc::Status GetMetadata(
+        grpc::string_ref, // serviceURL, 
+        grpc::string_ref, // methodName,
+        const grpc::AuthContext &,//channelAuthContext,
+        std::multimap<grpc::string, grpc::string> *metadata) override
+    {   
+        metadata->insert(std::make_pair("x-custom-auth-token", mToken));
+        return grpc::Status::OK;
+    }   
+//private:
+    grpc::string mToken;
+};
+
+std::shared_ptr<grpc::Channel>
+    createChannel(const UWaveServer::DataClient::GRPCOptions &options,
+                  spdlog::logger *logger)
+{
+    auto address = UWaveServer::DataClient::makeAddress(options);
+    auto serverCertificate = options.getServerCertificate();
+    if (serverCertificate)
+    {
+#ifndef NDEBUG
+        assert(!serverCertificate->empty());
+#endif
+        if (options.getAccessToken())
+        {
+            auto apiKey = *options.getAccessToken();
+#ifndef NDEBUG
+            assert(!apiKey.empty());
+#endif
+            SPDLOG_LOGGER_INFO(logger,
+                               "Creating secure channel with API key to {}",
+                               address);
+            auto callCredentials = grpc::MetadataCredentialsFromPlugin(
+                std::unique_ptr<grpc::MetadataCredentialsPlugin> (
+                    new ::CustomAuthenticator(apiKey)));
+            grpc::SslCredentialsOptions sslOptions;
+            sslOptions.pem_root_certs = *serverCertificate;
+            auto channelCredentials
+                = grpc::CompositeChannelCredentials(
+                      grpc::SslCredentials(sslOptions),
+                      callCredentials);
+            return grpc::CreateChannel(address, channelCredentials);
+        }
+        SPDLOG_LOGGER_INFO(logger,
+                           "Creating secure channel without API key to {}",
+                           address);
+        grpc::SslCredentialsOptions sslOptions;
+        sslOptions.pem_root_certs = *serverCertificate;
+        return grpc::CreateChannel(address,
+                                   grpc::SslCredentials(sslOptions));
+     }
+     SPDLOG_LOGGER_INFO(logger,
+                        "Creating non-secure channel to {}",
+                         address);
+     return grpc::CreateChannel(address, grpc::InsecureChannelCredentials());
+}
+
+class AsyncPacketSubscriber :
+    public grpc::ClientReadReactor<UDataPacketServiceAPI::V1::Packet>
+{
+public:
+/*
+    AsyncPacketSubscriber
+    (
+        UDataPacketServiceAPI::V1::Broadcast::Stub *stub,
+        const UDataPacketImportAPI::V1::SubscriptionRequest &request,
+        std::function<void (UDataPacketImportAPI::V1::Packet &&)> &addPacketCallback,
+        std::shared_ptr<spdlog::logger> logger,
+        std::atomic<bool> *keepRunning
+    ) :
+        mRequest(request),
+        mAddPacketCallback(addPacketCallback),
+        mLogger(logger),
+        mKeepRunning(keepRunning)
+    {
+        mClientContext.set_wait_for_ready(false); // Fail immediately if server isn't there
+        stub->async()->Subscribe(&mClientContext, &mRequest, this);
+        StartRead(&mPacket);
+        StartCall();
+    }
+
+    void OnReadDone(bool ok) override
+    {
+        if (ok)
+        {
+            mHadSuccessfulRead = true;
+            try
+            {
+                auto copy = mPacket;
+                mAddPacketCallback(std::move(copy));
+            }
+            catch (const std::exception &e)
+            {
+                SPDLOG_LOGGER_ERROR(mLogger,
+                                    "Failed to add packet to callback because {}",
+                                    std::string {e.what()});
+            }
+            if (!mKeepRunning->load())
+            {
+                //Finish(grpc::Status::OK);
+                mClientContext.TryCancel();
+            }
+            StartRead(&mPacket);
+        }
+        else
+        {
+            if (!mKeepRunning->load())
+            {
+                //Finish(grpc::Status::OK);
+                mClientContext.TryCancel();
+            }
+        }
+    }
+
+    void OnDone(const grpc::Status &status) override
+    {
+        std::unique_lock<std::mutex> lock(mMutex);
+        mStatus = status;
+        mDone = true;
+        mConditionVariable.notify_one();
+    }
+
+    [[nodiscard]] std::pair<grpc::Status, bool> await()
+    {
+        std::unique_lock<std::mutex> lock(mMutex);
+        mConditionVariable.wait(lock, [this] {return mDone;});
+        return std::pair{std::move(mStatus), mHadSuccessfulRead};
+    }
+
+#ifndef NDEBUG
+    ~AsyncPacketSubscriber()
+    {
+        SPDLOG_LOGGER_DEBUG(mLogger, "In destructor");
+    }
+#endif
+*/
+private:
+    grpc::ClientContext mClientContext;
+    UDataPacketServiceAPI::V1::SubscriptionRequest mRequest;
+    std::function
+    <
+        void (UDataPacketServiceAPI::V1::Packet &&packet)
+    > mAddPacketCallback;
+    std::shared_ptr<spdlog::logger> mLogger;
+    std::mutex mMutex;
+    std::condition_variable mConditionVariable;
+    UDataPacketServiceAPI::V1::Packet mPacket;
+    grpc::Status mStatus{grpc::Status::OK};
+    bool mDone{false};
+    std::atomic<bool> *mKeepRunning{nullptr};
+    bool mHadSuccessfulRead{false};
+};
 
 }
 
